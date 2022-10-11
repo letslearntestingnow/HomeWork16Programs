2f08b458dafc57c82594593c045dd0fe451a9c5f
Nachiketa Verma <letslearntestingnow@gmail.com>
2022-10-11 16:49:51 +0100

    Adding program City Name using Switch to the repo

---
+++ src/CityNameUsingSwitch.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class CityNameUsingSwitch
+{
+    public static void main(String[] args)
+    {
+        // Create scanner object
+        Scanner scannerObj = new Scanner(System.in);
+
+        // Get input
+        System.out.println("Enter an alphabet between a and f : ");
+        char cAlphabet = scannerObj.next().charAt(0);
+
+        switch (cAlphabet) {
+            case 'a', 'A' -> System.out.println("\nAhmedabad");
+            case 'b', 'B' -> System.out.println("\nBengaluru");
+            case 'c', 'C' -> System.out.println("\nCochin");
+            case 'd', 'D' -> System.out.println("\nDelhi");
+            case 'e', 'E' -> System.out.println("\nEnfield");
+            case 'f', 'F' -> System.out.println("\nFarnworth");
+            default -> System.out.println("\nInvalid entry");
+        }
+
+    }
+}
